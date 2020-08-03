8e149dfefdd0130fab8d9b88e992960cc7cd408b
Eric Hale <eric_hale@comcast.net>
2020-08-03 10:01:51 -0600

    Add funcntionalty to see a flight's basic info on it's page

--- spec/features/flights/show_spec.rb
+++ spec/features/flights/show_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "As a visitor, When I visit a flight's show page" do
   it "I see all of that flight's information" do
     airline = Airline.create(name: "Delta")
     flight = airline.flights.create(number: 1727, date: "08/03/20", time: "3:30pm MT", departure_city: "Denver", arrival_city: "Reno")
-    visit "/flights/:id"
+    visit "/flights/#{flight.id}"
 
     expect(page).to have_content(flight.number)
     expect(page).to have_content(flight.date)
