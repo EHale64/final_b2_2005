dbc2eb0f8967ad439d6c9c3eff9477c212dcb0fc
Eric Hale <eric_hale@comcast.net>
2020-08-03 09:35:43 -0600

    Add relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200803150924) do
+ActiveRecord::Schema.define(version: 20200803153018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,17 @@ ActiveRecord::Schema.define(version: 20200803150924) do
     t.string "arrival_city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "airline_id"
+    t.index ["airline_id"], name: "index_flights_on_airline_id"
+  end
+
+  create_table "passenger_flights", force: :cascade do |t|
+    t.bigint "passenger_id"
+    t.bigint "flight_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_passenger_flights_on_flight_id"
+    t.index ["passenger_id"], name: "index_passenger_flights_on_passenger_id"
   end
 
   create_table "passengers", force: :cascade do |t|
@@ -37,4 +48,7 @@ ActiveRecord::Schema.define(version: 20200803150924) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "flights", "airlines"
+  add_foreign_key "passenger_flights", "flights"
+  add_foreign_key "passenger_flights", "passengers"
 end
